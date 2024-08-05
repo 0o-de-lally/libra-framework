3ae7629a6c7d425c5a7b6738b3a4a853f2e1b543
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-08-05 16:04:38 -0400

    builds

--- tools/storage/src/db_from_snapshot.rs
+++ tools/storage/src/db_from_snapshot.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
 use diem_backup_cli::{
     backup_types::epoch_ending::restore::{EpochEndingRestoreController, EpochEndingRestoreOpt},
-    storage::{local_fs::LocalFs, BackupStorage, DBToolStorageOpt},
-    utils::{GlobalRestoreOpt, GlobalRestoreOptions, RestoreRunMode, TrustedWaypointOpt},
+    storage::{local_fs::LocalFs, BackupStorage},
+    utils::{GlobalRestoreOptions, RestoreRunMode, TrustedWaypointOpt},
 };
 use std::{path::PathBuf, sync::Arc};
 
@@ -24,21 +24,21 @@ pub fn trusted_waypoints(wp_str: &str) -> TrustedWaypointOpt {
 }
 
 
-pub async fn manifest_to_db(new_db_path: PathBuf, manifest_path: PathBuf, wp_string: &str) {
+pub async fn manifest_to_db(new_db_path: PathBuf, manifest_path: PathBuf, waypoint_str: &str) {
 
-    let epoch_restore_opts = restore_opts(manifest_path);
+    let epoch_restore_opts = restore_opts(manifest_path.to_str().expect("expect path str"));
     let global_restore_opts = GlobalRestoreOptions {
         run_mode: Arc::new(RestoreRunMode::Verify),
         target_version: 0,
         concurrent_downloads: 4,
         trusted_waypoints: Arc::new(
-            trusted_waypoints(wp_str)
+            trusted_waypoints(waypoint_str)
                 .verify()
                 .expect("could not verify waypoint"),
         ),
         replay_concurrency_level: 0,
     };
-    let db = init_storage(path).unwrap();
+    let db = init_storage(new_db_path).unwrap();
 
     EpochEndingRestoreController::new(epoch_restore_opts, global_restore_opts, db);
 }
@@ -46,9 +46,10 @@ pub async fn manifest_to_db(new_db_path: PathBuf, manifest_path: PathBuf, wp_str
 
 #[tokio::test]
 async fn try_read_manifest() {
-      let db_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let wp_str = "..";
-    let manifest_path = "tbd";
+    let crate_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let db_path = crate_dir.join("temp_db/");
+    let waypoint_str = "116:b4c9918ddb62469cc3e7e7b2a01b43aeac803470913b3a89afdcc44078df8d8a";
+    let manifest_path = crate_dir.join("fixtures/epoch_ending_116-.be9b");
 
-    manifest_to_db(db_path, wp_str, manifest_path);
+    manifest_to_db(db_path, manifest_path, waypoint_str).await;
 }
