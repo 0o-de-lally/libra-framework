816895918fb1cc1bf5c60e988ff40893a3efa824
xyz <xyz>
2024-08-18 17:08:55 +0000

    refactor twincli, breaking [skip ci]

--- testsuites/twin/src/runner.rs
+++ testsuites/twin/src/runner.rs
@@ -4,32 +4,58 @@ use std::{fs, path::PathBuf};
 
 /// Twin of the network
 #[derive(Parser)]
+pub struct TwinCli {
+  /// path of a working production db to become a test twin
+  #[clap(long, short)]
+  pub db_dir: PathBuf,
 
-/// Set up a twin of the network, with a synced db
-pub struct Twin {
-    /// path of snapshot db we want marlon to drive
-    #[clap(long, short)]
-    pub db_dir: PathBuf,
-    /// The operator.yaml file which contains registration information
-    #[clap(long, short)]
-    pub oper_file: Option<PathBuf>,
-    /// provide info about the DB state, e.g. version
-    #[clap(long, short)]
-    pub info: bool,
+  #[clap(subcommand)]
+  command: Sub
 
+}
+
+#[derive(clap::Subcommand)]
+pub enum Sub {
+  /// Run a swarm locally with the prepared db
+  Swarm {
     /// number of local validators to start
     #[clap(long, short)]
     pub count_vals: Option<u8>,
+  },
+  /// Just apply a change to DB at rest
+  Testnet {
+    #[clap(long, short)]
+    /// Pubkey files for the testnet validators which will drive the Twin
+    /// should point to validator-identity.yaml
+    pub val_id_files: Vec<PathBuf>
+  }
 }
-impl Twin {
+
+impl TwinCli {
     /// Runner for the twin
     pub async fn run(&self) -> anyhow::Result<(), anyhow::Error> {
         let db_path = fs::canonicalize(&self.db_dir)?;
 
-        let num_validators = self.count_vals.unwrap_or(1);
+        match self.command {
+          Sub::Swarm {count_vals} => {
+            let num_validators = count_vals.unwrap_or(1);
 
-        let mut smoke = LibraSmoke::new(Some(num_validators), None).await?;
-        Twin::make_twin_swarm(&mut smoke, Some(db_path), true).await?;
+            let mut smoke = LibraSmoke::new(Some(num_validators), None).await?;
+            Twin::make_twin_swarm(&mut smoke, Some(db_path), true).await?;
+          },
+          Sub::Testnet {val_id_files} => {
+            let creds = validtor_registration::parse_pub_files_to_vec(val_id_files);
+            println!("Creating a rescue blob from the reference db");
+
+            let rescue_blob_path = Self::make_rescue_twin_blob(&db_path, creds).await?;
+
+            println!("3. Apply the rescue blob to the swarm db & bootstrap");
+
+            let wp = Self::apply_rescue_on_db(&temp_db_path, &rescue_blob_path)?;
+          }
+        }
+
+
 
         Ok(())
     }
