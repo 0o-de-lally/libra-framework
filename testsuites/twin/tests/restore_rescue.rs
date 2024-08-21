60ca75b64b3015b5afe49c40eb794ae481140717
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-08-21 18:39:05 -0400

    test e2e restore and rescue

--- testsuites/twin/tests/restore_rescue.rs
+++ testsuites/twin/tests/restore_rescue.rs
@@ -1,6 +1,7 @@
 use diem_types::transaction::Transaction;
 use diem_types::transaction::WriteSetPayload;
 use libra_rescue::session_tools::{self, libra_run_session, writeset_voodoo_events};
+use libra_storage::utils;
 use libra_storage::{restore, restore_bundle::RestoreBundle};
 use libra_twin_tests::runner::Twin;
 use std::path::{Path, PathBuf};
@@ -17,6 +18,7 @@ fn make_rescue_noop(db_path: &Path) -> anyhow::Result<PathBuf> {
 
     let bytes = bcs::to_bytes(&gen_tx)?;
     std::fs::write(&out, bytes.as_slice())?;
+
     Ok(out)
 }
 
@@ -28,33 +30,38 @@ async fn test_helper_setup_restore() -> anyhow::Result<PathBuf> {
         .unwrap() //testsuites
         .parent()
         .unwrap(); //root
-    let mut b = RestoreBundle::new(workspace.join("tools/storage/fixtures/v7"));
+    let fixtures_path = workspace.join("tools/storage/fixtures/v7");
+    let mut b = RestoreBundle::new(fixtures_path.clone());
     b.load().unwrap();
     let mut db_temp = diem_temppath::TempPath::new();
     db_temp.persist();
     db_temp.create_as_dir()?;
 
     restore::full_restore(db_temp.path(), &b).await?;
+
+    // db would not yet have been bootstrapped with a genesis tx
+    utils::bootstrap_without_node(db_temp.path(), fixtures_path.join("genesis.blob").as_ref())?;
+
     Ok(db_temp.path().to_owned())
 }
 
 #[tokio::test]
-async fn test_full_restore() -> anyhow::Result<()> {
-    // let db_temp = test_helper_setup_restore().await?;
+async fn test_full_restore_e2e() -> anyhow::Result<()> {
+    let db_temp = test_helper_setup_restore().await?;
     // let db_temp = Path::new("/tmp/fa27fe959d023d6efc0163f0ae71f47b");
     // let db_temp = Path::new("/root/.libra/rescue_db");
-    let db_temp = Path::new("/root/.libra/data/db");
+    // let db_temp = Path::new("/root/.libra/data/db");
 
     dbg!(&db_temp);
 
     println!("Create a rescue blob from the reference db");
 
     // let rescue_blob_path = Twin::make_rescue_twin_blob(&temp_db_path, creds).await?;
-    let rescue_blob_path = make_rescue_noop(db_temp)?;
+    let rescue_blob_path = make_rescue_noop(&db_temp)?;
 
     println!("Apply the rescue blob to the swarm db & bootstrap");
 
-    let wp = Twin::apply_rescue_on_db(db_temp, &rescue_blob_path)?;
-
+    let wp = Twin::apply_rescue_on_db(&db_temp, &rescue_blob_path)?;
+    dbg!(&wp);
     Ok(())
 }
