92ebde87940059bd90873166301c0617a6455a3c
Peregrine Fitz Leveret <peregrine_fitz_leveret@github.com>
2025-02-03 11:46:06 -0500

    rename to smoke

--- testsuites/smoke/src/main.rs
+++ testsuites/smoke/src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use libra_swarm::cli::SwarmCli;
+use libra_smoke::cli::SwarmCli;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     SwarmCli::parse().run().await?;
--- upgrade-tests/tests/twin_upgrades.rs
+++ upgrade-tests/tests/twin_upgrades.rs
@@ -2,8 +2,8 @@ mod support;
 
 use anyhow::Context;
 use libra_framework::release::ReleaseTarget;
+use libra_smoke::twin::Twin;
 use libra_smoke_tests::libra_smoke::LibraSmoke;
-use libra_swarm::twin::Twin;
 // Here we are testing how a Twin modified swarm responds to an upgrade
 // upgrades are being applied against Mainnet data which is recovered by a snapshot.
 
