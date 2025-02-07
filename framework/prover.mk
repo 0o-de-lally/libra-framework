8ab8605a345f833508c756092fd5ee815a2734a1
Gabi LeStaccato <gabi_lestaccato@github.com>
2025-02-07 16:17:03 -0500

    try with stdlib

--- framework/prover.mk
+++ framework/prover.mk
@@ -6,17 +6,19 @@ PROVER_TESTS = demo ol_account slow sacred
 VENDOR_TESTS = chain_id guid
 
 # Formal verification of each framework using the Move prover
-prove:
+prove-stdlib:
 	@cd move-stdlib && \
 	echo "Testing move-stdlib" && \
 	find sources -type f -name "*.move" ! -name "*.spec.move" | sed 's/\.move$$//' | \
 	xargs -I {} sh -c 'echo "Testing file: {}"; libra move prove -f {} || echo "Error in file: {}"'
 
+prove-vendor:
 	@cd vendor-stdlib && \
 	echo "Testing vendor-stdlib" && \
 	find sources -type f -name "*.move" ! -name "*.spec.move" | sed 's/\.move$$//' | \
 	xargs -I {} sh -c 'echo "Testing file: {}"; libra move prove -f {} || echo "Error in file: {}"'
 
+prove-libra:
 	@cd libra-framework && \
 	for i in ${PROVER_TESTS} ${VENDOR_TESTS}; do \
 			libra move prove -f $$i; \
