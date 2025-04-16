6681474e996a167404765be65e8d79dcc880ec63
Nella MacPiano <nella_macpiano@github.com>
2025-04-16 13:37:30 -0400

    patch merge

--- tools/testnet/src/replace_validators_file.rs
+++ tools/testnet/src/replace_validators_file.rs
@@ -27,6 +27,7 @@ pub async fn replace_validators_blob(
         db_path,
         creds,
         &upgrade_mrb_path,
+        None
     )?;
 
     let gen_tx = Transaction::GenesisTransaction(WriteSetPayload::Direct(cs));
